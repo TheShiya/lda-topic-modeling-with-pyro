53bb688fad322828d0d64829ea8e29c0e32f35f4
Shiya <shiya@keplerfi.com>
2020-11-27 21:38:21 -0500

    renamed to util.py

--- util.py
+++ util.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import os
+from nltk.corpus import stopwords
+
+stopwords = set(stopwords.words('english'))
 
 ORDER_PATH = os.path.join("data", "order_products__train.csv")
 PRODUCT_PATH = os.path.join("data", "product_name.csv")
+
 
 def load_process(order_path=ORDER_PATH, product_path=PRODUCT_PATH):
     o = pd.read_csv(order_path).dropna()
@@ -20,4 +24,4 @@ def load_process(order_path=ORDER_PATH, product_path=PRODUCT_PATH):
 
     data = o.groupby("order_id")["shortname"].apply(list)
     data = data[(data.apply(len) >= 10) & (data.apply(len) <= 50)]
-    return data
+    return o, data
